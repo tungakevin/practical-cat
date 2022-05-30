8a67201da8538501325a002cd28c5b2647f28727
tungakevin <88825179+tungakevin@users.noreply.github.com>
2022-05-30 19:51:48 +0200

    Add files via upload

---
+++ logout.php
@@ -0,0 +1,19 @@
+<?php
+/* 
+| Developed by: Tunga kevin
+
+ */ 
+
+require_once 'config.php';
+
+unset($_SESSION['facebook_access_token']);
+
+
+unset($_SESSION['fb_user_id']);
+unset($_SESSION['fb_user_name']);
+unset($_SESSION['fb_user_email']);
+unset($_SESSION['fb_user_pic']);
+
+
+header("Location:index.php");
+?>
